cd26ddbdb37d3d0233d62b4732a403fbd0f3fc6f
Akash Yadav <akashcse7054@gmail.com>
2025-06-30 11:52:16 +0530

    exploring get,post.delete

---
+++ homework.md
@@ -0,0 +1,22 @@
+- create repository
+- Initialised Repository
+- node_modules, package.json,package-lock.json
+- install express
+- create a server 
+- listen to port 7000
+- Write request handlers for /test.. /hello
+- Install nodemon and update script inside package.json
+- What are Dependencies
+- what is the use of "-g" while installing npm
+- Difference between caret and tilda (~ vs ^)
+
+
+- initialize git
+- .gitignore
+- Create a remote repo on github
+- Push all the code to remote origin
+- Play with routes and route extensions exe. /hello, /test,/hello/2, /xyz
+- Order of routes matter a lot
+- Install postman app and make a workspace/collection> test API call
+- Write logic to handle GET,POST,PATCH,DELETE API calls and test them on postman
+
--- src/app.js
+++ src/app.js
@@ -2,20 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res) =>{
-    res.send("hey! everyone")
+
+// this will only handle GET call to/user 
+app.get("/user",(req,res) =>{
+    res.send("Get the successfully data of user")
+});
+
+app.post("/user",(req,res) =>{
+    res.send("Post the successfully data from server")
 })
 
-app.use("/test", (req,res) => {
-    res.send('Namaste dev');
+app.delete("/user",(req,res) =>{
+    res.send("deleted successfully");
+})
+
+// this will match all the HTTP method API calls to /test
+app.use("/hello/2",(req,res) =>{
+    res.send("abracadabra");
 })
 
 app.use("/hello", (req,res) => {
     res.send("Hello from the server");
 })
 
-// app.use("/hello", (res,req) => {
-//     res.send('Namaste dev');
+app.use("/test", (req,res) => {
+    res.send('Namaste NodeJs test is start');
+})
+
+// app.use("/",(req,res) =>{
+//     res.send("hey! everyone")
 // })
 
 app.listen(7000,() =>{
