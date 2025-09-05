05d460779012272a69ce81a53c00e2a0b8836abe
Akash Yadav <akashcse7054@gmail.com>
2025-09-05 13:56:20 +0530

    create userAuth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,25 +1,36 @@
-const adminAuth = (req, res, next) =>{
-    console.log("Admin auth is getting checked!!");
-    const token = "xyz";
-    const isAdminAuthorid = token === "xyz";
-    if(!isAdminAuthorid){
-       res.status(401).send("Unauthorized requst");
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+const userAuth = async(req, res, next) =>{
+
+    try{
+
+    const {token} = req.cookies;
+
+    if(!token){
+        throw new Error("Invalid token");
     }
-    else{
-        next();
+
+    const decodeObj = await jwt.verify(token, "dev@123akas$&");
+
+    const { _id } = decodeObj;
+
+    const user = await User.findById( _id );
+
+    if(!user){
+        throw new Error("user not found");
     }
+
+    req.user = user;
+    
+    next();
+    }
+    catch(err) {
+    res.status(404).send("Error : " + err.message);
+    }
+
 };
 
-const userAuth = (req, res, next) =>{
-    console.log("User auth is getting checked");
-    const token = "xyz";
-    const isUserAuthorized = token === "xyz";
-    if(!isUserAuthorized){
-        res.status(401).send("Unauthorized User");
-    }
-    else{
-        next()
-    }
-};
+module.exports = {userAuth};
 
-module.exports = {adminAuth, userAuth};
